67a8f0a4d8a76e41baf71405c6e3dcbc33ba2869
zacrobat <zac@portable.com.au>
2016-05-17 10:52:28 +1000

    Add wp php after_setup_theme method for modifying shortcodes in a child theme

--- learnings.php
+++ learnings.php
@@ -13,3 +13,37 @@ docker run -it php:alpine php -a
 ctr-d to escape back to zsh
 https://hub.docker.com/_/php/
 Next: download php locally and run in Atom. http://php.net/manual/en/install.macosx.php */
+
+// Using after_setup_theme
+// This goes in child theme functions.php - replace a shortcode with a new one that has an amended function.
+// https://wordpress.org/support/topic/override-themes-shortcodesphp
+// Add bio to team section if needed :
+
+add_action( 'after_setup_theme', 'adv_child_theme_setup' );
+
+function adv_child_theme_setup() {
+   remove_shortcode( 'krown_team' );
+   add_shortcode( 'krown_team', 'adv_krown_team_function' );
+}
+
+function adv_krown_team_function( $atts, $content ) {
+
+    extract( shortcode_atts( array(
+        'el_class'  => '',
+        'image' 	=> '',
+        'title'  => '',
+        'subtitle'  => '',
+        'bio'  => '' // bio added for this project - see $bio below as well.
+    ), $atts ) );
+
+    $html = '<div class="krown-team' . ( $el_class != '' ? ' ' . $el_class : '' ) . ' clearfix">
+      <img src="' . aq_resize( $image, '400' ) . '" alt="' . $title . '" />
+      <h4>' . $title . '</h4>
+      <h5>' . $subtitle . '</h5>
+      <p>' . $bio . '</p>
+      <div class="content">' . do_shortcode( $content ) . '</div>
+    </div>';
+
+   return $html;
+
+}
