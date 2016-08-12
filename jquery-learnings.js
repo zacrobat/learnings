ed6e55e438e6030c4ed0dcd3c071b4cc6d696020
Zac Cobb <zacrobat55@gmail.com>
2016-08-12 12:13:25 +1000

    Add jQuery sandpit, jQuery codecademy, jQuery codecamp learnings

--- jquery-learnings.js
+++ jquery-learnings.js
@@ -1,6 +1,37 @@
 $(document).ready(function() {
-  $('div').slideDown('slow');
-  //four tabs, not superdry
+  //getting dry
+  $('.tabs a').on('click', function() {
+    $('ul.tabs li a').removeClass('active-tab');
+    var name = $(this).attr('for');
+    console.log(name);
+    var completeName = '[name="' + name + '"]';
+    console.log(completeName);
+    $(completeName).removeClass('invisible').addClass('visible').siblings().removeClass('visible').addClass('invisible');
+  });
+  // four tabs made with jQuery addClass
+  $('#tab01').click(function() {
+    $('#tabcontent01').siblings().addClass('invisible');
+    $('#tabcontent01').removeClass('invisible').addClass('visible');
+  });
+  $('#tab02').click(function() {
+    $('ul.tabs li a').removeClass('active-tab');
+    $('#tabcontent02').siblings().addClass('invisible');
+    $('#tabcontent02').removeClass('invisible').addClass('visible');
+  });
+  $('#tab03').click(function() {
+    $('ul.tabs li a').removeClass('active-tab');
+    $('.tab').siblings().addClass('inactive-tab');
+    $('#tabcontent03').siblings().addClass('invisible');
+    $('#tabcontent03').removeClass('invisible').addClass('visible');
+  });
+  $('#tab04').click(function() {
+    $('ul.tabs li a').removeClass('active-tab');
+    $('.tab').siblings().addClass('inactive-tab');
+    $('#tabcontent04').siblings().addClass('invisible');
+    $('#tabcontent04').removeClass('invisible').addClass('visible');
+  });
+
+  //four tabs, not superdry, made with jQuery .show and .hide
   $('#tab01').click(function() {
     $('#tabcontent01').show();
     $('#tabcontent02').hide();
